3210d0da3480174abf99722fb595f831b13fda02
phdljr <phdljr@naver.com>
2022-12-05 01:59:03 +0900

    refactor: LocalDateTime으로 변경

--- src/main/java/kr/ac/soup/dto/BoardResponseDto.java
+++ src/main/java/kr/ac/soup/dto/BoardResponseDto.java
@@ -5,7 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -16,5 +16,5 @@ public class BoardResponseDto {
     private String nickname;
     private String title;
     private String content;
-    private LocalDate registerDate;
+    private LocalDateTime registerDate;
 }
