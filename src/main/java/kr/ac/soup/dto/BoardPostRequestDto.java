757226de3384e3f961bcef9e500749d1fc190ef9
phdljr <phdljr@naver.com>
2022-12-05 22:12:41 +0900

    test: Board CRUD 테스트 케이스 추가

---
+++ src/main/java/kr/ac/soup/dto/BoardPostRequestDto.java
@@ -0,0 +1,16 @@
+package kr.ac.soup.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardPostRequestDto {
+    private Long memberId;
+    private String title;
+    private String content;
+}
