eaed88e7acd78777fea9ece31fc4fcdb602d062d
phdljr <phdljr@naver.com>
2022-12-06 21:12:28 +0900

    test: Reply CRUD 테스트 케이스 추가 및 테스트 코드 상속 실험

---
+++ src/test/java/kr/ac/soup/repository/ReplyRepositoryTest.java
@@ -0,0 +1,27 @@
+package kr.ac.soup.repository;
+
+import kr.ac.soup.config.web.WebConfig;
+import kr.ac.soup.entity.Reply;
+import kr.ac.soup.setup.SetUpTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringJUnitWebConfig(classes = {WebConfig.class})
+class ReplyRepositoryTest extends SetUpTest {
+
+    @Test
+    @DisplayName("게시글에 등록된 댓글의 개수를 가져온다.")
+    @Transactional
+    void findAllByBoardIdOrderByRegisterDateAsc() {
+        createDummyData();
+        List<Reply> replyList = replyRepository.findAllByBoardIdOrderByRegisterDateAsc(1L);
+
+        assertThat(replyList.size()).isEqualTo(5);
+    }
+}
